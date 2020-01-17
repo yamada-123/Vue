99b0861da5455917e507b62e2c15c2dd4947980b
satoshi yamada <yasain0852@yahoo.co.jp>
2020-01-17 16:46:46 +0900

    修正

--- test.js
+++ test.js
@@ -1,37 +1,61 @@
 new Vue({
   // このelにHTMLのidを指定することで、そのidのDOMでVue.jsのインスタンスがインポートされる
-  el: '#app',
+  el: "#app",
   // dataは、定義されたVue.jsのインスタンスが持つ属性（値）
   data: {
-    name: 'A山B郎',
-    cource: 'Webエンジニアコース',
-    acceptancePeriod: '2019年01月期',
-    defaultLastId: 3,
-    students: [{
+    newstudentText: "",
+    students: [
+      {
         id: 1,
-        name: '野呂浩良',
-        cource: '機械学習コース',
-        acceptancePeriod: '2019年01月期'
+        name: "野呂浩良",
+        cource: "機械学習コース",
+        acceptancePeriod: "2019年01月期"
       },
       {
         id: 2,
-        name: '富永修司',
-        cource: 'Webエンジニアコース',
-        acceptancePeriod: '2017年11月期'
+        name: "富永修司",
+        cource: "Webエンジニアコース",
+        acceptancePeriod: "2017年11月期"
       },
       {
         id: 3,
-        name: '斉藤一起',
-        cource: 'Webエンジニアコース',
-        acceptancePeriod: '2017年11月期'
+        name: "斉藤一起",
+        cource: "Webエンジニアコース",
+        acceptancePeriod: "2017年11月期"
       }
-    ]
+    ],
+    nextstudentId: 4
   },
   // methodsに、Vue.jsのインスタンスに使用させたいメソッドを記載する
   methods: {
-    addStudent: function () {
+    addStudent: function() {
+      this.students.push({
+        id: this.nextstudentId++,
+        name: this.newstudentText,
+        cource: this.cource,
+        acceptancePeriod: this.acceptancePeriod
+      });
+      this.newstudentText = "";
+      this.cource = "";
+      this.acceptancePeriod = "";
       // この function() { } の中にaddStudentメソッド
       // （入力欄に入力された値と、ID値（最後に登録されたID値+1の値）が新たに出現する機能）の処理を書く
     }
   }
-})
+});
+
+//   // methodsに、Vue.jsのインスタンスに使用させたいメソッドを記載する
+//   methods: {
+//     addStudent: function () {
+//       this.students.push({
+//         id: this.students.length + 1,
+//         name: this.name,
+//         cource: this.cource,
+//         acceptancePeriod: this.acceptancePeriod
+//       });
+//       this.name = '';
+//       this.cource = '';
+//       this.acceptancePeriod = '';
+//     }
+//   }
+// })
